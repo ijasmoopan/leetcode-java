77b380e222cd595d272b2f4122d6c56ca2f1d812
Mohamad Ijas M <ijasmoopan46@gmail.com>
2024-06-03 02:15:07 +0530

    solution(1004): O(n) time complexity

---
+++ Arrays/Max Consecutive Ones III/Solution.java
@@ -0,0 +1,49 @@
+// 1004. Max Consecutive Ones III (Medium)
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 };
+        int k = 2;
+        int solution = longestOnes(nums, k);
+        System.out.println("Longes ones: " + solution);
+    }
+
+    public static int longestOnes(int[] nums, int k) {
+        int l = 0, r = 0, maxLen = 0, zeros = 0;
+        while (r < nums.length) {
+            if (nums[r] == 0)
+                zeros++;
+            if (zeros > k) {
+                if (nums[l] == 0)
+                    zeros--;
+                l++;
+            } else {
+                maxLen = Math.max(maxLen, r - l + 1);
+            }
+            r++;
+        }
+        return maxLen;
+    }
+}
+
+/*
+ * 
+ * 1004. Max Consecutive Ones III (Medium)
+ * Given a binary array nums and an integer k, return the maximum number of
+ * consecutive 1's in the array if you can flip at most k 0's.
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+ * Output: 6
+ * Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+ * Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ * 
+ * Example 2:
+ * 
+ * Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+ * Output: 10
+ * Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+ * Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ * 
+ */
